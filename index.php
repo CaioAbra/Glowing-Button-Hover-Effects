2045a3bc6332b1c236ef4114c38267ecb3cd8870
Caio Matheus dos Santos Abra <caioabra41@gmail.com>
2022-05-16 23:13:17 -0300

    Adicionando tag author

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="config/css/index.css">
+    <meta name="author" content="Caio Matheus dos Santos Abra">
 </head>
 <body>
     <a href="#"><span>Button</span><i></i></a>
